1dd56b07773149e85e08462f7c3b8473f610ea19
Sant'Clear <santclear@gmail.com>
2021-05-04 20:57:47 -0300

    Criado variáveis de entradas, saidas esperadas, pesos e epocas na implementação de rede neural multicamada.

--- redes neurais - implementacoes sem frameworks/03 - implementacao - rede neural multicamada.py
+++ redes neurais - implementacoes sem frameworks/03 - implementacao - rede neural multicamada.py
@@ -9,3 +9,26 @@ import numpy as numpy
 
 def sigmoid(soma):
     return 1 / (1 + numpy.exp(-soma))
+
+entradas = numpy.array([
+    [0,0],
+    [0,1],
+    [1,0],
+    [1,1]])
+
+saidasEsperadas = numpy.array([
+    [0],
+    [1],
+    [1],
+    [0]])
+
+pesosEntradaCamadaOculta = numpy.array([
+    [-0.424, -0.740, -0.961],
+    [0.358, -0.577, -0.469]])
+    
+pesosSaidaCamadaOculta = numpy.array([
+    [-0.017],
+    [-0.893],
+    [0.148]])
+
+epocas = 100
