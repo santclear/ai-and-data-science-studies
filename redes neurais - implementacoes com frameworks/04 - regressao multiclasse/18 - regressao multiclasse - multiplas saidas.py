b632f21914d38ba36580f4022b3b55f180495de5
Sant'Clear <santclear@gmail.com>
2021-05-15 01:59:36 -0300

    Implementação de rede neural com regressão e multiplas saídas.

---
+++ redes neurais - implementacoes com frameworks/04 - regressao multiclasse/18 - regressao multiclasse - multiplas saidas.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 01:22:04 2021
+
+@author: santc
+"""
+
+import pandas as pd
+
+import tensorflow as tf # atualizado: tensorflow==2.0.0-beta1
+from tensorflow.keras.layers import Dense, Dropout, Activation, Input 
+from tensorflow.keras.models import Model
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+# Base obtida do site: kaggle.com
+base = pd.read_csv('games.csv')
+
+################################################
+### PREPROCESSAMENTO: Exclusão de colunas (axis = 1) que não serão necessárias no treinamento e teste,
+### após esse processamento a base terá 13 atributos (colunas)
+base = base.drop('Other_Sales', axis = 1)
+base = base.drop('Global_Sales', axis = 1)
+base = base.drop('Developer', axis = 1)
+
+# Excluí todas as linhas (axis = 0) que possui a o valor nan
+base = base.dropna(axis = 0)
+# Mantém os dados maior que 1 e exclui o restante
+base = base.loc[base['NA_Sales'] > 1]
+base = base.loc[base['EU_Sales'] > 1]
