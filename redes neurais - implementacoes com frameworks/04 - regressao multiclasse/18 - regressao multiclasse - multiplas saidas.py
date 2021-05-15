a147edc30f2c7f9d6add6a441f579c8fe20a432c
Sant'Clear <santclear@gmail.com>
2021-05-15 02:59:49 -0300

    Criado rede e treinamento na "Implementação de rede neural com regressão e uma saída".

--- redes neurais - implementacoes com frameworks/04 - regressao multiclasse/18 - regressao multiclasse - multiplas saidas.py
+++ redes neurais - implementacoes com frameworks/04 - regressao multiclasse/18 - regressao multiclasse - multiplas saidas.py
@@ -7,8 +7,7 @@ Created on Sat May 15 01:22:04 2021
 
 import pandas as pd
 
-import tensorflow as tf # atualizado: tensorflow==2.0.0-beta1
-from tensorflow.keras.layers import Dense, Dropout, Activation, Input 
+from tensorflow.keras.layers import Dense, Input 
 from tensorflow.keras.models import Model
 
 from sklearn.preprocessing import OneHotEncoder
@@ -39,3 +38,37 @@ base['Name'].value_counts()
 nomeJogos = base.Name
 # Excluí a coluna Name
 base = base.drop('Name', axis = 1)
+# Pega as colunas desejadas
+previsores = base.iloc[:, [0,1,2,3,7,8,9,10,11]].values
+# Como não é problema de classificação, as variáveis são definidas com o nome de
+# valores que se deseja prever através de aproximação numérica.
+# Pega as colunas NA Sales, EU Sales e JP Sales  (multi tipos (3) de valores para
+# treinamento)
+vendaNA = base.iloc[:, 4].values
+vendaEU = base.iloc[:, 5].values
+vendaJP = base.iloc[:, 6].values
+
+################################################
+### PREPROCESSAMENTO: Converte dados numéricos em Onehot Encoder
+# Transforma as categorias 0, 2, 3, 8 em algo parecido com:
+# 0: 1 0 0 ... 0 
+# 2: 0 1 0 ... 0
+# 3: 0 0 1 ... 0
+# ... ...
+# onehot encoder pode ser usado quando não há uma ordem de importância entre 
+# os atributos, por exemplo o tipo de câmbio não pode ser afirmado que "manual 
+# é maior que automático" e vice-versa
+onehotencoder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [0,2,3,8])],remainder='passthrough')
+previsores = onehotencoder.fit_transform(previsores).toarray()
+
+camadaEntrada = Input(shape=(61,))
+camadaOculta1 = Dense(units = 32, activation = 'sigmoid')(camadaEntrada)
+camadaOculta2 = Dense(units = 32, activation = 'sigmoid')(camadaOculta1)
+camadaSaida1 = Dense(units = 1, activation = 'linear')(camadaOculta2)
+camadaSaida2 = Dense(units = 1, activation = 'linear')(camadaOculta2)
+camadaSaida3 = Dense(units = 1, activation = 'linear')(camadaOculta2)
+
+regressor = Model(inputs = camadaEntrada, outputs = [camadaSaida1, camadaSaida2, camadaSaida3])
+regressor.compile(optimizer = 'adam', loss = 'mse')
+regressor.fit(previsores, [vendaNA, vendaEU, vendaJP], epochs = 5000, batch_size = 100)
+previsaoNA, previsaoEU, previsaoJP = regressor.predict(previsores)
