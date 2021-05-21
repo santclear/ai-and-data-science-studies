055cbd5fe367596cfcd8fdef7b3c74ec900fdb63
Sant'Clear <santclear@gmail.com>
2021-05-21 12:06:06 -0300

    Processamento de dados na "Implementação de rede neural recorrente, previsão de preço de ação da bolsa de valores".

--- redes neurais - implementacoes com frameworks/06 - recorrente/28 - lstm - previsao preco acao.py
+++ redes neurais - implementacoes com frameworks/06 - recorrente/28 - lstm - previsao preco acao.py
@@ -24,3 +24,25 @@ baseTreinamento = base.iloc[:, 1:2].values
 # dimui o custo de processamento
 normalizador = MinMaxScaler(feature_range=(0,1))
 baseTreinamentoNormalizada = normalizador.fit_transform(baseTreinamento)
+
+previsores = []
+precoReal = []
+
+# 90: Índice inicial 
+for i in range(90, 1242):
+    # i-90:i -> Índice 0 até i(90 registros iniciais) da coluna 0
+    previsores.append(baseTreinamentoNormalizada[i-90:i,0])
+    precoReal.append(baseTreinamentoNormalizada[i, 0])
+
+previsores, precoReal = np.array(previsores), np.array(precoReal)
+
+# batch_size
+# 1152 registros
+qtdRegistros = previsores.shape[0]
+# timesteps
+# 90 intervalos
+intervalosTempo = previsores.shape[1]
+# 1 atributo previsor -> Preço de abertura Open
+# input_dim
+qtdAtributosPrevisores = 1
+previsores = np.reshape(previsores, (qtdRegistros, intervalosTempo, qtdAtributosPrevisores))
