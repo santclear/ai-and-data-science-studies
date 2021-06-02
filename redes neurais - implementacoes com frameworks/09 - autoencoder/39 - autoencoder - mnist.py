85d992aef76f478d947a5d201598be42ae7ce815
Sant'Clear <santclear@gmail.com>
2021-06-01 21:25:59 -0300

    Treinamento e teste na "Implementações de rede neural autoencoder para reduz de dimensionalidade, usando a base mnist".

--- redes neurais - implementacoes com frameworks/09 - autoencoder/39 - autoencoder - mnist.py
+++ redes neurais - implementacoes com frameworks/09 - autoencoder/39 - autoencoder - mnist.py
@@ -39,3 +39,57 @@ dimensoesPrevisoresTeste = previsoresTeste.shape[1:]
 # 784
 dimensoesPrevisoresMutiplicadaTeste = np.prod(dimensoesPrevisoresTeste)
 previsoresTeste = previsoresTeste.reshape((qtdRegistrosPrevisoresTeste, dimensoesPrevisoresMutiplicadaTeste))
+
+# Camada entrada: 784 neurônios (valor dos pixels)
+# Camada oculta (para reduz de dimensinalidade (encode)): 32 neurônios (valor dos pixels)
+# Camada de saída (decode): 784 neurônios (valor dos pixels)
+fatorCompactacao = 784 / 32
+
+autoencoder = Sequential()
+autoencoder.add(Dense(units = 32, activation = 'relu', input_dim = 784))
+autoencoder.add(Dense(units = 784, activation = 'sigmoid'))
+autoencoder.summary()
+autoencoder.compile(optimizer = 'adam', loss = 'binary_crossentropy',
+                    metrics = ['accuracy'])
+
+# 1º parâmetro X: previsores
+# 2º parâmetro y: classes (nesses caso, como a rede terá apenas o objetivo de
+# treinar a codificação e decodificação ao invés de classificação e como 
+# não é permitido passar nulo, será passado o mesmo parâmetro declarado 
+# anteriormente, previsores). Previsão supervisionado por si própria, característica
+# principal de uma rede com finalidade de autoencoder
+autoencoder.fit(previsoresTreinamento, previsoresTreinamento, epochs = 50, 
+				batch_size = 256, validation_data = (previsoresTeste, previsoresTeste))
+
+dimensaoOriginal = Input(shape=(784,))
+camadaEncoder = autoencoder.layers[0]
+encoder = Model(dimensaoOriginal, camadaEncoder(dimensaoOriginal))
+encoder.summary()
+
+imagensCodificadas = encoder.predict(previsoresTeste)
+imagensDecodificadas = autoencoder.predict(previsoresTeste)
+
+numeroImagens = 10
+imagensTeste = np.random.randint(previsoresTeste.shape[0], size = numeroImagens)
+plt.figure(figsize=(18,18))
+for i, indiceImagem in enumerate(imagensTeste):
+    #print(i)
+    #print(indiceImagem)
+    
+    # imagem original
+    eixo = plt.subplot(10,10,i + 1)
+    plt.imshow(previsoresTeste[indiceImagem].reshape(28,28))
+    plt.xticks(())
+    plt.yticks(())
+    
+    # imagem codificada
+    eixo = plt.subplot(10,10,i + 1 + numeroImagens)
+    plt.imshow(imagensCodificadas[indiceImagem].reshape(8,4))
+    plt.xticks(())
+    plt.yticks(())
+    
+     # imagem reconstruída
+    eixo = plt.subplot(10,10,i + 1 + numeroImagens * 2)
+    plt.imshow(imagensDecodificadas[indiceImagem].reshape(28,28))
+    plt.xticks(())
+    plt.yticks(())
